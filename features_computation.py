237fb367911b1378748c0ae75d19bf84fc0b90bc
JulesSanchez <jules.n.sanchez@gmail.com>
2020-02-01 16:54:40 +0100

    finished implemented shape distributions

--- features_computation.py
+++ features_computation.py
@@ -1,11 +1,31 @@
 
 import numpy as np
-
 # Import functions from scikit-learn
 from sklearn.neighbors import KDTree
-
-
+from physt import h1, h2, histogramdd
+import time 
 EPSILON = 1e-10 
+
+def tetrahedron_calc_volume(a, b, c, d):
+    test = np.abs(np.linalg.det(np.stack(((a-b).T , (b-c).T, (c-d).T)).T))/6
+    return test
+
+def area(a, b, c) :
+    return 0.5 * np.linalg.norm( np.cross( b-a, c-a ), axis=1 )
+
+def get_angle(a, b, c):
+    ba = a - b
+    bc = c - b
+
+    cosine_angle = np.diag(np.inner(ba, bc)) / (np.linalg.norm(ba) * np.linalg.norm(bc))
+    angle = np.arccos(cosine_angle)
+    return angle
+
+def adaptative_bin(samples,bins):
+    total_number = len(samples)
+    bin_s = [np.percentile(samples,i*10) for i in range(11)]
+    return np.array(bin_s)
+
 
 def local_PCA(points):
 
@@ -47,3 +67,58 @@ def compute_covariance_features(query_points, cloud_points, tree, radius):
     sumeigen = np.sum(all_eigenvalues,axis=1)
     change_curvature = all_eigenvalues[:,0]/(sumeigen+EPSILON)
     return verticality, linearity, planarity, sphericity, omnivariance, anisotropy, eigenentropy, sumeigen, change_curvature
+
+def shape_distributions(query_points, cloud_points, tree, radius=2.5, pulls=255, bins=10):
+    #Pulls is the number of time we compute shape distributions for each points
+
+    D1 = np.zeros((query_points.shape[0],255))
+    D2 = np.zeros((query_points.shape[0],255))
+    D3 = np.zeros((query_points.shape[0],255))
+    D4 = np.zeros((query_points.shape[0],255)) 
+    A3 = np.zeros((query_points.shape[0],255))
+
+    for i in range(query_points.shape[0]):
+        local_D4 = np.zeros(pulls) 
+        local_A3 = np.zeros(pulls)
+        point = query_points[i]
+        _, indices, _ = tree.search_radius_vector_3d(point, radius)
+        neighbors = cloud_points[indices[1:],:]
+        if len(neighbors)< 4*pulls:
+            _, indices, _ = tree.search_knn_vector_3d(point, knn=4*pulls+1)
+            neighbors = cloud_points[indices[1:],:]
+        length = np.arange(len(neighbors))
+
+        indicesD1 = np.random.choice(length,size=pulls,replace=False)
+        indicesD2 = np.random.choice(length,size=(2,pulls),replace=False)
+        indicesD3 = np.random.choice(length,size=(3,pulls),replace=False)
+        indicesD4 = np.random.choice(length,size=(4,pulls),replace=False)
+        indicesA3 = np.random.choice(length,size=(3,pulls),replace=False)
+        centroid = np.mean(neighbors,axis=0)
+        D1[i] = np.linalg.norm(neighbors[indicesD1]-centroid,axis=1)
+        D2[i] = np.linalg.norm(neighbors[indicesD2[0,:]]-neighbors[indicesD2[1,:]],axis=1)
+        D3[i] = area(neighbors[indicesD3[0,:]],neighbors[indicesD3[1,:]],neighbors[indicesD3[2,:]])
+        D4[i] = tetrahedron_calc_volume(neighbors[indicesD4[0]],neighbors[indicesD4[1]],neighbors[indicesD4[2]],neighbors[indicesD4[3]])
+        A3[i] = get_angle(neighbors[indicesA3[0]],neighbors[indicesA3[1]],neighbors[indicesA3[2]])
+
+    if isinstance(bins, int):
+        old_bins = bins
+        bins = []
+        bins.append(adaptative_bin(D1.flatten(),old_bins))
+        bins.append(adaptative_bin(D2.flatten(),old_bins))
+        bins.append(adaptative_bin(D3.flatten(),old_bins))
+        bins.append(adaptative_bin(D4.flatten(),old_bins))
+        bins.append(adaptative_bin(A3.flatten(),old_bins))
+        D1 = np.array([np.histogram(D1[i],bins=bins[0])[0] for i in range(len(D1))]).T
+        D2 = np.array([np.histogram(D2[i],bins=bins[1])[0] for i in range(len(D2))]).T
+        D3 = np.array([np.histogram(D3[i],bins=bins[2])[0] for i in range(len(D3))]).T
+        D4 = np.array([np.histogram(D4[i],bins=bins[3])[0] for i in range(len(D4))]).T
+        A3 = np.array([np.histogram(A3[i],bins=bins[4])[0] for i in range(len(A3))]).T
+    else:
+        D1 = np.array([np.histogram(D1[i],bins=bins[0])[0] for i in range(len(D1))]).T
+        D2 = np.array([np.histogram(D2[i],bins=bins[1])[0] for i in range(len(D2))]).T
+        D3 = np.array([np.histogram(D3[i],bins=bins[2])[0] for i in range(len(D3))]).T
+        D4 = np.array([np.histogram(D4[i],bins=bins[3])[0] for i in range(len(D4))]).T
+        A3 = np.array([np.histogram(A3[i],bins=bins[4])[0] for i in range(len(A3))]).T
+
+    return D1, D2, D3, D4, A3, bins
+
--- main.py
+++ main.py
@@ -1,38 +1,69 @@
 from loader import load_point_cloud, cross_val
-from features_computation import compute_covariance_features
+from features_computation import compute_covariance_features, shape_distributions
 from subsampler import get_even_number
-import os
+import os, math
 import numpy as np 
 import xgboost as xgb
 import pickle
 
 PATH = 'data/MiniChallenge/training'
+PATH_TEST = 'data/MiniChallenge/test'
 EXTENSION = '.ply'
 SIZE = 1000
-RADIUS = 0.5
+RADIUS_COV = 0.5
+RADIUS_SHAPE = 1.5
+BINS = 10
+PULLS = 255
 CLASSES = ['Unclassified','Ground','Building','Poles','Pedestrians','Cars','Vegetation']
+import time 
 
 if __name__ == '__main__':
 
     data_cross_val = cross_val()
     classifiers = []
     best_score = 0
-    best_classifier = None
+    best_classifier = 0
 
     for k in range(len(data_cross_val)):
 
         data_local = data_cross_val[k]
         train_cloud1, train_label1, tree1 = load_point_cloud(os.path.join(PATH,data_local['training'][0])+EXTENSION)
         train_cloud2, train_label2, tree2 = load_point_cloud(os.path.join(PATH,data_local['training'][1])+EXTENSION)
+        t1 = time.time()
         new_train_cloud1, new_train_label1 = get_even_number(train_cloud1,train_label1,SIZE)
+        print('Time to subsample ' +data_local['training'][0] + ' : ' + str(time.time() - t1) )
+        t1 = time.time()
         new_train_cloud2, new_train_label2 = get_even_number(train_cloud2,train_label2,SIZE)
+        print('Time to subsample ' +data_local['training'][1] + ' : ' + str(time.time() - t1) )
 
-        verticality, linearity, planarity, sphericity, omnivariance, anisotropy, eigenentropy, sumeigen, change_curvature = compute_covariance_features(new_train_cloud1,train_cloud1,tree1,radius=RADIUS)
-        features_1 = np.vstack((verticality, linearity, planarity, sphericity, omnivariance, anisotropy, eigenentropy, sumeigen, change_curvature)).T
-        verticality, linearity, planarity, sphericity, omnivariance, anisotropy, eigenentropy, sumeigen, change_curvature = compute_covariance_features(new_train_cloud2,train_cloud2,tree2,radius=RADIUS)
-        features_2 = np.vstack((verticality, linearity, planarity, sphericity, omnivariance, anisotropy, eigenentropy, sumeigen, change_curvature)).T
+        t1 = time.time()
+
+        verticality, linearity, planarity, sphericity, omnivariance, anisotropy, eigenentropy, sumeigen, change_curvature = compute_covariance_features(new_train_cloud1,train_cloud1,tree1,radius=RADIUS_COV)
+        print('Time to compute Cov Features for ' +data_local['training'][0] + ' : ' + str(time.time() - t1) )
+
+        print('Cov Features 1 computed')
+        t1 = time.time()
+        A1, A2, A3, A4, D3, BINS = shape_distributions(new_train_cloud1,train_cloud1,tree1,RADIUS_SHAPE,PULLS,BINS)
+        print('Time to compute Shape Features for ' +data_local['training'][0] + ' : ' + str(time.time() - t1) )
+        print('Shape Features 1 computed')
+        features_1_cov = np.vstack((verticality, linearity, planarity, sphericity, omnivariance, anisotropy, eigenentropy, sumeigen, change_curvature)).T
+        features_1_shape = np.vstack((A1, A2, A3, A4, D3)).T
+        features_1 = np.append(features_1_cov, features_1_shape,axis=1)
+        
+        t1 = time.time()
+        verticality, linearity, planarity, sphericity, omnivariance, anisotropy, eigenentropy, sumeigen, change_curvature = compute_covariance_features(new_train_cloud2,train_cloud2,tree2,radius=RADIUS_COV)
+        print('Time to compute Cov Features for ' +data_local['training'][1] + ' : ' + str(time.time() - t1) )
+        print('Cov Features 2 computed')
+        t1 = time.time()
+        A1, A2, A3, A4, D3, _ = shape_distributions(new_train_cloud2,train_cloud2,tree2,RADIUS_SHAPE,PULLS,BINS)
+        print('Time to compute Shape Features for ' +data_local['training'][1] + ' : ' + str(time.time() - t1) )
+        print('Shape Features 2 computed')
+        features_2_cov = np.vstack((verticality, linearity, planarity, sphericity, omnivariance, anisotropy, eigenentropy, sumeigen, change_curvature)).T
+        features_2_shape = np.vstack((A1, A2, A3, A4, D3)).T
+        features_2 = np.append(features_2_cov, features_2_shape,axis=1)
         
         features = np.append(features_1,features_2,axis=0)
+        print(features.shape)
         labels = np.append(new_train_label1,new_train_label2,axis=0)
 
         classifier = xgb.XGBClassifier()
@@ -43,20 +74,26 @@ if __name__ == '__main__':
         indices = val_label > 0
         new_val_cloud = val_cloud[indices]
         new_val_label = val_label[indices]
-        verticality, linearity, planarity, sphericity, omnivariance, anisotropy, eigenentropy, sumeigen, change_curvature = compute_covariance_features(new_val_cloud,val_cloud,tree,radius=RADIUS)
-        features_test = np.vstack((verticality, linearity, planarity, sphericity, omnivariance, anisotropy, eigenentropy, sumeigen, change_curvature)).T
+        verticality, linearity, planarity, sphericity, omnivariance, anisotropy, eigenentropy, sumeigen, change_curvature = compute_covariance_features(new_val_cloud,val_cloud,tree,radius=RADIUS_COV)
+        A1, A2, A3, A4, D3, _ = shape_distributions(new_val_cloud,val_cloud,tree,RADIUS_SHAPE,PULLS,BINS)
+        features_test_cov = np.vstack((verticality, linearity, planarity, sphericity, omnivariance, anisotropy, eigenentropy, sumeigen, change_curvature)).T
+        features_test_shape = np.vstack((A1, A2, A3, A4, D3)).T
+        features_test = np.append(features_test_cov, features_test_shape,axis=1)
         val_score = classifier.score(features_test,new_val_label)
         print('Validation accuracy : ' +str(val_score))
-        for k in range(1,len(CLASSES)):
-            indices = new_val_label == k
+        for i in range(1,len(CLASSES)):
+            indices = new_val_label == i
             local_val_score = classifier.score(features_test[indices],new_val_label[indices])
-            print('Validation accuracy for label ' + CLASSES[k] +' : '  +str(local_val_score))
+            if math.isnan(local_val_score):
+                continue
+            print('Validation accuracy for label ' + CLASSES[i] +' : '  +str(local_val_score))
 
 
         classifiers.append(classifier)
         if val_score > best_score:
             best_classifier = k
             best_score = val_score
+        break
     
-    pickle.dump(classifiers[best_classifier], open(str(SIZE) + 'Kclassifier.pickle','wb'))
+    pickle.dump(classifiers[best_classifier], open(str(SIZE/1000) + 'Kclassifier.pickle','wb'))
 
