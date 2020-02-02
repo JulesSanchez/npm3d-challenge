c4402dec976d88f1cd0c69917452c88de10957da
ManifoldFR <manifoldfr@outlook.com>
2020-02-02 10:40:26 +0100

    change feedforward network structure

--- models/deep_stdfeatures.py
+++ models/deep_stdfeatures.py
@@ -18,17 +18,27 @@ class Net(nn.Module):
         self.batchnorm = nn.BatchNorm1d(3)
         self.lin1 = nn.Sequential(
             nn.Linear(input_size, hidden_size),
-            nn.LeakyReLU()
+            nn.ReLU(),
         )
-        self.lin2 = nn.Linear(hidden_size, OUT_SIZE)
-        self.out = nn.LogSoftmax(dim=1)  # this works with NLLLoss
         
+        self.linglob = nn.Sequential(
+            nn.Linear(hidden_size, 128),
+            nn.ReLU()
+        )
+        
+        self.out = nn.Sequential(
+            nn.Linear(hidden_size+128, OUT_SIZE),
+            nn.LogSoftmax(dim=1)  # this works with NLLLoss
+        )
 
     def forward(self, points: Tensor, x: Tensor):
+        n_pts = points.shape[0]
         points = self.batchnorm(points)
         z = torch.cat((points, x), 1)
         z = self.lin1(z)
-        z = self.lin2(z)
-        y = self.out(z)
-        return y
+        globfeat = self.linglob(z)
+        globfeat = torch.mean(globfeat, dim=0, keepdim=True)  # (1,128)
+        z = torch.cat((z, globfeat.repeat(n_pts, 1)), 1)  # (n_pts,H+128)
+        output = self.out(z)
+        return output
 
--- train_nn_stdfeat.py
+++ train_nn_stdfeat.py
@@ -9,7 +9,7 @@ import tqdm
 from utils.loader import MyPointCloud, ConcatPointClouds
 from sklearn.metrics import accuracy_score
 
-SAMPLING_SIZE = 2000
+SAMPLING_SIZE = 4000
 
 
 def train(model: Net, optim: torch.optim.Optimizer, criterion, dataset):
@@ -34,9 +34,8 @@ def train(model: Net, optim: torch.optim.Optimizer, criterion, dataset):
     
     prediction = np.argmax(output.data.numpy(), axis=1)
     accuracy = accuracy_score(labels.data.numpy(), prediction)
-    # import ipdb; ipdb.set_trace()
     
-    print("Train loss: %.3e. Train accuracy: %.2f" % (loss, 100*accuracy))
+    return loss.data.numpy(), accuracy
     
 
 def validate(model: Net, criterion, val_dataset):
@@ -63,7 +62,7 @@ def save_checkpoint(epoch, model: nn.Module, optimizer, loss, path):
 
 
 NUM_EPOCHS = 80
-VALID_EVERY = 5
+VALID_EVERY = 4
 
 
 if __name__ == "__main__":
@@ -71,21 +70,49 @@ if __name__ == "__main__":
     dataset2 = MyPointCloud("data/MiniChallenge/training/MiniParis1.ply")
     train_dataset = ConcatPointClouds([dataset1, dataset2])
     val_dataset = MyPointCloud("data/MiniChallenge/training/MiniLille2.ply")
-    # import ipdb; ipdb.set_trace()
-    num_features = 9 + 3
-    model = Net(num_features)
+    num_features = 9
+    model = Net(num_features + 3)
     optimizer = optim.SGD(model.parameters(), lr=0.06, momentum=.8)
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min')
     criterion = torch.nn.NLLLoss()
     
+    
+    losses_ = []
+    accuracies_ = []
+    val_losses_ = []
+    val_accuracies_ = []
 
     for epoch in tqdm.tqdm(range(NUM_EPOCHS)):
         print("Epoch %d:" % epoch, end=' ')
-        train(model, optimizer, criterion, train_dataset)
+        loss, accuracy = train(model, optimizer, criterion, train_dataset)
         
+        losses_.append(loss)
+        accuracies_.append(accuracy)
+        
+        print("Train loss: %.3e. Train accuracy: %.2f" % (loss, 100*accuracy))
+
 
         if (epoch+1) % VALID_EVERY == 0:
             val_loss, val_acc = validate(model, criterion, val_dataset)
             print("\tVal loss: %.3e. Val accuracy: %.2f"
                   % (val_loss, 100*val_acc))
             scheduler.step(val_loss)
+
+            val_losses_.append(val_loss)
+            val_accuracies_.append(val_acc)
+    
+    import matplotlib.pyplot as plt
+    
+    # import ipdb; ipdb.set_trace()
+    fig = plt.figure(figsize=(8, 6))
+    plt.subplot(211)
+    plt.plot(np.arange(NUM_EPOCHS), losses_, lw=1, label="train_loss")
+    plt.plot(np.linspace(0, NUM_EPOCHS, len(val_losses_)), val_losses_, lw=1, label="val_loss")
+    plt.legend()
+    
+    plt.subplot(212)
+    plt.plot(np.arange(NUM_EPOCHS), accuracies_, lw=1, label="train_acc")
+    plt.plot(np.linspace(0, NUM_EPOCHS, len(val_losses_)), val_accuracies_, lw=1, label="val_acc")
+    plt.legend()
+
+    plt.show()
