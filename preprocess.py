4317c89bd1c648b36299729b923c9ab942f25fb1
ManifoldFR <manifoldfr@outlook.com>
2020-02-03 17:19:22 +0100

    working voxelization preprocess script

--- preprocess.py
+++ preprocess.py
@@ -4,11 +4,14 @@ Preprocess the point clouds by voxel downsampling.
 import os
 from utils.loader import preprocess, NAMEFILES, NAMETEST
 
+VOXEL_SIZE = 0.5
+
 for filename in NAMEFILES:
-    print("Preprocessing %s" % filename)
-    basename = os.path.splitext(os.path.basename(filename))[0]
-    basename += "_voxelized.ply"
-    
-    preprocess()
+    filepath = os.path.join("data/MiniChallenge/training", filename)
+    print("Preprocessing %s" % filepath)
+    preprocess(filepath+".ply", filepath+"_voxelized", VOXEL_SIZE)
 
-
+for filename in NAMETEST:
+    filepath = os.path.join("data/MiniChallenge/test", filename)
+    print("Preprocessing %s" % filepath)
+    preprocess(filepath+".ply", filepath+"_voxelized", VOXEL_SIZE, labels=False)
